f854866869a04344f07b7a1b6c82866109d29e67
StuartFarmer <email@stuartfarmer.me>
2015-07-28 14:50:44 -0400

    southwest demo responding to enter and exits

--- Geofencing Demo/ViewController.m
+++ Geofencing Demo/ViewController.m
@@ -36,12 +36,38 @@
     [mapView addOverlay:[StateHelper MKPolygonForColorado]];
     [mapView addOverlay:[StateHelper MKPolygonForNewMexico]];
     
-    fencing = [[Geofencing alloc] init];
+    // Create an array of the geofences you want to monitor
+    NSMutableArray *southWesternStates = [[NSMutableArray alloc] init];
+    GFGeofence *utah = [[GFGeofence alloc] initFromMKPolygon:[StateHelper MKPolygonForUtah] andIdentifier:@"Utah"];
+    [southWesternStates addObject:utah];
     
-    [fencing monitorRegions:@[[StateHelper MKPolygonForUtah],[StateHelper MKPolygonForArizona],[StateHelper MKPolygonForColorado],[StateHelper MKPolygonForNewMexico]] onEnter:^(NSArray *regions) {
-        NSLog(@"You're in Utah, baby!");
+    GFGeofence *arizona = [[GFGeofence alloc] initFromMKPolygon:[StateHelper MKPolygonForArizona] andIdentifier:@"Arizona"];
+    [southWesternStates addObject:arizona];
+    
+    GFGeofence *colorado = [[GFGeofence alloc] initFromMKPolygon:[StateHelper MKPolygonForColorado] andIdentifier:@"Colorado"];
+    [southWesternStates addObject:colorado];
+    
+    GFGeofence *newMexico = [[GFGeofence alloc] initFromMKPolygon:[StateHelper MKPolygonForNewMexico] andIdentifier:@"New Mexico"];
+    [southWesternStates addObject:newMexico];
+    
+    // Set up a Geofencing object and start monitoring
+    fencing = [[Geofencing alloc] init];
+    [fencing monitorRegions:southWesternStates onEnter:^(NSArray *regions) {
+        // Handle and identify fence enter events
+        for (GFGeofence *fence in regions) {
+            if ([fence.identifier isEqual:@"Utah"]) NSLog(@"Welcome to Utah!");
+            else if ([fence.identifier isEqual:@"Arizona"]) NSLog(@"Welcome to Arizona!");
+            else if ([fence.identifier isEqual:@"Colorado"]) NSLog(@"Welcome to Colorado!");
+            else if ([fence.identifier isEqual:@"New Mexico"]) NSLog(@"Welcome to New Mexico!");
+        }
     } onExit:^(NSArray *regions) {
-        NSLog(@"You left Utah.");
+        // Handle and identify fence exit events
+        for (GFGeofence *fence in regions) {
+            if ([fence.identifier isEqual:@"Utah"]) NSLog(@"See you next time in Utah!");
+            else if ([fence.identifier isEqual:@"Arizona"]) NSLog(@"See you next time in Arizona!");
+            else if ([fence.identifier isEqual:@"Colorado"]) NSLog(@"See you next time in Colorado!");
+            else if ([fence.identifier isEqual:@"New Mexico"]) NSLog(@"See you next time in New Mexico!");
+        }
     }];
     
 }
