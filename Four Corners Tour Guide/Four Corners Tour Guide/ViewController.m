880fb15fcbfb8bfee9d3e69e578c0cd766c0dbad
StuartFarmer <email@stuartfarmer.me>
2015-07-28 16:06:08 -0400

    finished ui template

--- Four Corners Tour Guide/Four Corners Tour Guide/ViewController.m
+++ Four Corners Tour Guide/Four Corners Tour Guide/ViewController.m
@@ -17,18 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    mapView = [[MKMapView alloc] initWithFrame:self.view.frame];
-    
-    mapView.delegate = self;
-    mapView.showsUserLocation = true;
-    
-    [self.view addSubview:mapView];
-    
-    [mapView addOverlay:[StateHelper MKPolygonForUtah]];
-    [mapView addOverlay:[StateHelper MKPolygonForArizona]];
-    [mapView addOverlay:[StateHelper MKPolygonForColorado]];
-    [mapView addOverlay:[StateHelper MKPolygonForNewMexico]];
-    
     // Create an array of the geofences you want to monitor
     NSMutableArray *southWesternStates = [[NSMutableArray alloc] init];
     GFGeofence *utah = [[GFGeofence alloc] initFromMKPolygon:[StateHelper MKPolygonForUtah] andIdentifier:@"Utah"];
@@ -68,20 +56,6 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-#pragma MapKit Delegate Methods
-- (MKOverlayView *)mapView:(MKMapView *)mapView viewForOverlay:(id<MKOverlay>)overlay
-{
-    if ([overlay isKindOfClass:[MKPolygon class]])
-    {
-        MKPolygonView* aView = [[MKPolygonView alloc]initWithPolygon:(MKPolygon*)overlay];
-        aView.fillColor = [[UIColor cyanColor] colorWithAlphaComponent:0.2];
-        aView.strokeColor = [[UIColor blueColor] colorWithAlphaComponent:0.7];
-        aView.lineWidth = 3;
-        return aView;
-    }
-    return nil;
 }
 
 #pragma UI Update Methods
