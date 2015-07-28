5ddae9611ce5ea8b50e4848604e30cb4271ec4f6
StuartFarmer <email@stuartfarmer.me>
2015-07-28 16:40:20 -0400

    ui updates when user in new state works

--- Four Corners Tour Guide/Four Corners Tour Guide/ViewController.h
+++ Four Corners Tour Guide/Four Corners Tour Guide/ViewController.h
@@ -10,6 +10,15 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@property (strong, nonatomic) IBOutlet UIImageView *flagImageView;
+@property (strong, nonatomic) IBOutlet UILabel *capitalLabel;
+@property (strong, nonatomic) IBOutlet UILabel *populationLabel;
+@property (strong, nonatomic) IBOutlet UILabel *stateBirdLabel;
+@property (strong, nonatomic) IBOutlet UITextView *factsTextView;
+@property (strong, nonatomic) IBOutlet UIImageView *backgroundImageView;
+
+- (IBAction)moreInfoPressed:(id)sender;
 
 @end
 
--- Four Corners Tour Guide/Four Corners Tour Guide/ViewController.m
+++ Four Corners Tour Guide/Four Corners Tour Guide/ViewController.m
@@ -8,6 +8,7 @@
 @interface ViewController () <MKMapViewDelegate> {
     Geofencing *fencing;
     MKMapView *mapView;
+    NSString *currentState;
 }
 
 @end
@@ -36,21 +37,10 @@
     [fencing monitorRegions:southWesternStates onEnter:^(NSArray *regions) {
         // Handle and identify fence enter events
         for (GFGeofence *fence in regions) {
-            if ([fence.identifier isEqual:@"Utah"]) NSLog(@"Welcome to Utah!");
-            else if ([fence.identifier isEqual:@"Arizona"]) NSLog(@"Welcome to Arizona!");
-            else if ([fence.identifier isEqual:@"Colorado"]) NSLog(@"Welcome to Colorado!");
-            else if ([fence.identifier isEqual:@"New Mexico"]) NSLog(@"Welcome to New Mexico!");
+            currentState = fence.identifier;
+            [self updateUIForState:currentState];
         }
-    } onExit:^(NSArray *regions) {
-        // Handle and identify fence exit events
-        for (GFGeofence *fence in regions) {
-            if ([fence.identifier isEqual:@"Utah"]) NSLog(@"See you next time in Utah!");
-            else if ([fence.identifier isEqual:@"Arizona"]) NSLog(@"See you next time in Arizona!");
-            else if ([fence.identifier isEqual:@"Colorado"]) NSLog(@"See you next time in Colorado!");
-            else if ([fence.identifier isEqual:@"New Mexico"]) NSLog(@"See you next time in New Mexico!");
-        }
-    }];
-    
+    } onExit:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -60,7 +50,45 @@
 
 #pragma UI Update Methods
 - (void)updateUIForState:(NSString *)identifier {
-    
+    if ([identifier isEqual:@"Utah"]) {
+        self.backgroundImageView.image = [UIImage imageNamed:@"utahBg.jpg"];
+        self.titleLabel.text = @"Welcome to Utah!";
+        self.flagImageView.image = [UIImage imageNamed:@"utahFlag.png"];
+        self.capitalLabel.text = @"Capital: Salt Lake City";
+        self.populationLabel.text = @"Population: 2.943 million";
+        self.stateBirdLabel.text = @"State bird: California Gull";
+        self.factsTextView.text = @"Utah is a western state defined by its vast expanses of desert and the Wasatch Range mountains. Salt Lake City, the capital, is centered around Temple Square, headquarters of the Mormon Church and site of its majestic temple and tabernacle, with its massive dome and renowned choir. The Great Salt Lake’s buoyant waters attract swimmers and sunbathers, especially at Antelope Island State Park.";
+    }
+    else if ([identifier isEqual:@"Arizona"]) {
+        self.backgroundImageView.image = [UIImage imageNamed:@"arizonaBg.jpg"];
+        self.titleLabel.text = @"Welcome to Arizona!";
+        self.flagImageView.image = [UIImage imageNamed:@"arizonaFlag.png"];
+        self.capitalLabel.text = @"Capital: Pheonix";
+        self.populationLabel.text = @"Population: 6.731 million";
+        self.stateBirdLabel.text = @"State bird: Cactus Wren";
+        self.factsTextView.text = @"Arizona, a southwestern U.S. state, is best known for its reliably sunny weather and as home to the the Grand Canyon, the mile-deep chasm carved by the Colorado River. Many visitors utilize Flagstaff, a Ponderosa Pine-covered mountain town, as a gateway to the Grand Canyon. Other natural features include the saguaro cactus-filled Sonoran Desert and the rugged formations of Red Rock State Park.";
+    }
+    else if ([identifier isEqual:@"Colorado"]) {
+        self.backgroundImageView.image = [UIImage imageNamed:@"coloradoBg.jpg"];
+        self.titleLabel.text = @"Welcome to Colorado!";
+        self.flagImageView.image = [UIImage imageNamed:@"coloradoFlag.jpg"];
+        self.capitalLabel.text = @"Capital: Denver";
+        self.populationLabel.text = @"Population: 5.356 million";
+        self.stateBirdLabel.text = @"State bird: Lark Bunting";
+        self.factsTextView.text = @"Colorado, a western U.S. state, is known for its geographic diversity, with snow-covered mountains, arid desert lands and river canyons. Opportunities for outdoor recreation include kayaking the Colorado River, hiking the nearly 500-mile Colorado Trail and skiing at resorts such as Vail and Aspen. Perched a mile above sea level, Denver, Colorado’s capital and largest city, features a vibrant downtown area.";
+    }
+    else if ([identifier isEqual:@"New Mexico"]) {
+        self.backgroundImageView.image = [UIImage imageNamed:@"newMexicoBg.jpg"];
+        self.titleLabel.text = @"Welcome to New Mexico!";
+        self.flagImageView.image = [UIImage imageNamed:@"newMexicoFlag.png"];
+        self.capitalLabel.text = @"Capital: Santa Fe";
+        self.populationLabel.text = @"Population: 2.086 million";
+        self.stateBirdLabel.text = @"State bird: Greater Roadrunner";
+        self.factsTextView.text = @"New Mexico is a southwestern state whose diverse terrain encompasses the Chihuahuan Desert and the Sangre de Cristo Mountains. Its capital, Santa Fe, founded in 1610, is known for upscale spas and Spanish colonial architecture. It's also home to a vibrant arts scene, as well as the Georgia O’Keeffe Museum, with its iconic New Mexican landscape paintings, and the open-air Santa Fe Opera.";
+    }
 }
 
+- (IBAction)moreInfoPressed:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://en.wikipedia.org/wiki/%@", currentState]]];
+}
 @end
